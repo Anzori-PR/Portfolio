86d085e9e1e8b3baca128d77fe1b7ff58afc5475
Anzori-PR <saxelashvilianzori@gmail.com>
2025-05-04 23:03:13 +0400

    Add Projects, skills, add contact form functionality, add transitions & fonts

--- app.js
+++ app.js
@@ -1,89 +1,125 @@
-AOS.init();
+AOS.init(
+  {once: true}
+);
 
 
 const navbar = document.querySelector('.custom-navbar');
 const navbarLogo = document.querySelector('.logo');
 const navbarLinks = document.querySelectorAll('.link');
 const burgerm = document.querySelector('.burgerm');
-const navbarHeight = 20;  // Adjust this value based on your navbar height;
-
-
+const navbarHeight = 20;
 
 window.addEventListener('scroll', () => {
-    console.log(window.scrollY, navbarHeight);
-    if (window.scrollY > navbarHeight) {
-        navbar.style.backgroundColor = "white"
-        navbar.style.borderBottom = "1px solid rgba(222, 226, 230, 0.34)"
-        navbar.style.height = "65px"
-        navbarLogo.style.color = "#695aa6";
-        navbarLinks.forEach(link => {
-            link.style.color = "#6c757d";
-        });
-        burgerm.style.color = "#6c757d";
-    } else {
-        navbar.style = 'transparent';
-        navbar.style.height = "100px"
-        navbar.style.borderBottom = "none"
-        navbarLogo.style.color = "white";
-        navbarLinks.forEach(link => {
-          link.style.color = "White";
-      });
-        burgerm.style.color = "white";
-    }
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add('fixednav');
+    navbarLogo.style.color = "#695aa6";
+    navbarLinks.forEach(link => {
+      link.style.color = "#6c757d";
+    });
+    burgerm.style.color = "#6c757d";
+  } else {
+    navbar.classList.remove('fixednav');
+    navbarLogo.style.color = "white";
+    navbarLinks.forEach(link => {
+      link.style.color = "White";
+    });
+    burgerm.style.color = "white";
+  }
 });
-
 
 
 const projects = [
-    {
-        name: "Project 1",
-        description: "This is the first project. It is a simple web application that does X, Y, and Z.",
-        image: "https://via.placeholder.com/150",
-        link: "https://example.com/project1",
-        technologies: ["HTML", "CSS", "JavaScript"],
-    }
+  {
+    name: "Geo-Jobs",
+    description: "Geo-Jobs is a user-friendly job search platform designed to help you find job opportunities in Georgia! 🚀",
+    image: "./assets/project/geo-jobs.png",
+    link: "https://geo-jobss.vercel.app/Dashboard",
+    technologies: ["Angular", "Tailwind", "Node.js", "MongoDB"],
+  },
+  {
+    name: "Ecommerce-JS",
+    description: "Welcome to my E-commerce Website project repository! This project aims to create a simple e-commerce website using HTML, CSS, and JavaScript. 🛒💻",
+    image: "./assets/project/shopper.png",
+    link: "https://anzori-pr.github.io/Ecommerce-JS/index.html",
+    technologies: ["HTML", "CSS", "JavaScript", "firebase"],
+  },
+  {
+    name: "ExploreEra",
+    description: "ExploreEra is a fictional travel website that allows users to discover and book unique travel experiences around the world. 🌍✈️",
+    image: "./assets/project/ExploreEra.png",
+    link: "https://anzori-pr.github.io/Unilab-Project/Home/index.html",
+    technologies: ["HTML", "CSS", "JavaScript", "Figma"],
+  },
+  {
+    name: "Online Store",
+    description: "This is a simple eCommerce website project built with Angular. It features a front-end interface for browsing products, adding them to the cart, and managing the cart.🛍️",
+    image: "./assets/project/angular-Ecommerce.png",
+    link: "https://e-commerces-iota.vercel.app/Home",
+    technologies: ["Angular", "typescript", "Tailwind", "Local Storage"],
+  },
+  {
+    name: "Qalaqobana",
+    description: "Qalaqobana is a Georgian-language multiplayer browser game where players take turns coming up with words in various categories (e.g., cities, names, objects) based on a randomly selected letter.📒",
+    image: "./assets/project/qalaqobana.png",
+    link: "https://qalaqobana.vercel.app/Home",
+    technologies: ["Angular", "Tailwind", "Node.js", "MongoDB", "Socket.io"],
+  },
+  {
+    name: "Rocket Launches",
+    description: "This is an Angular project that fetches and displays real-time data from the SpaceX API. The application provides detailed information about upcoming and past rocket launches, missions, launchpads, and rockets using a clean and responsive UI. 🚀",
+    image: "./assets/project/rocket-launch.png",
+    link: "https://rocket-launches-sooty.vercel.app/Home",
+    technologies: ["Angular", "Tailwind", "Rest API", "vercel"],
+  },
 ]
 
-const scrollContainer = document.querySelector('.project-scroll-container');
-const scrollTrack = document.getElementById('scroll-track');
-const leftscroll = document.querySelector('.leftscroll');
-const rightscroll = document.querySelector('.rightscroll');
+const projectsContainer = document.querySelector('.projects-grid');
 
-const card = document.querySelector('.project-card');
-const scrollAmount = card.offsetWidth + 50;
-
-// Duplicate cards for seamless loop
-scrollTrack.innerHTML += scrollTrack.innerHTML;
-
-// Manual scroll
-leftscroll.addEventListener('click', () => {
-  scrollContainer.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+projects.forEach(project => {
+  const projectCard = document.createElement('a');
+  projectCard.href = project.link;
+  projectCard.target = "_blank";
+  projectCard.classList.add('project-card');
+  projectCard.innerHTML = `
+          <div class="project-image">
+              <img src="${project.image}" alt="Project 1">
+          </div>
+          <div class="project-info">
+              <div class="project-tags">
+                  ${project.technologies.map(tech => `<span class="project-tag">${tech}</span>`).join('')}
+              </div>
+              <h3 class="project-title">${project.name}</h3>
+              <p class="project-description">${project.description}</p>
+          </div>
+    `;
+  projectsContainer.appendChild(projectCard);
 });
-rightscroll.addEventListener('click', () => {
-  scrollContainer.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+
+
+// Email Validation
+
+const submitBtn = document.getElementById('submit');
+const response = document.getElementById('response');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   submitBtn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_2fm4ume';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      submitBtn.value = 'Send Email';
+      response.innerHTML = 'Email sent successfully!';
+      form.reset();
+      setTimeout(() => {
+        response.innerHTML = '';
+      }, 3000);
+    }, (err) => {
+      submitBtn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
 });
-
-// Auto scroll
-let autoScrollInterval;
-function startAutoScroll() {
-  autoScrollInterval = setInterval(() => {
-    scrollContainer.scrollLeft += 1;
-
-    if (scrollContainer.scrollLeft >= scrollTrack.scrollWidth / 2) {
-      scrollContainer.scrollLeft = 0;
-    }
-  }, 20);
-}
-
-function stopAutoScroll() {
-  clearInterval(autoScrollInterval);
-}
-
-startAutoScroll();
-
-scrollContainer.addEventListener('mouseenter', stopAutoScroll);
-scrollContainer.addEventListener('mouseleave', startAutoScroll);
-leftscroll.addEventListener('mouseenter', stopAutoScroll);
-leftscroll.addEventListener('mouseleave', startAutoScroll);
-rightscroll.addEventListener('mouseenter', stopAutoScroll);
-rightscroll.addEventListener('mouseleave', startAutoScroll);
